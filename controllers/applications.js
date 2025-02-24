8ee701aeae5d436f1f4856e9b35caa1b8896124d
Jad Aoun <103859749+JadAoun1@users.noreply.github.com>
2025-02-24 17:10:56 -0500

    Initial commit

---
+++ controllers/applications.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+////////
+//ROUTES
+////////
+
+/////////////
+//INDEX ROUTE
+/////////////
+
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('applications/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+
+module.exports = router;
